60a8ca3fb702b9ead7da9c13ba65bb2132445dcd
Rafael García <Rafael Garca>
2016-11-25 13:28:22 +0100

    cambio en test

--- proy1/src/test/java/testing/RandomTest.java
+++ proy1/src/test/java/testing/RandomTest.java
@@ -2,6 +2,7 @@ package testing;
 
 import org.junit.Test;
 
+import domain.Car;
 import io.github.benas.randombeans.EnhancedRandomBuilder;
 import io.github.benas.randombeans.api.EnhancedRandom;
 
@@ -10,8 +11,10 @@ public class RandomTest {
 	//Libreria random-beans
 	@Test
 	public void test(){
-		EnhancedRandom random = EnhancedRandomBuilder.aNewEnhancedRandomBuilder().build();
-		System.out.println(random.nextInt());
+		EnhancedRandom enhancedRandom = EnhancedRandomBuilder.aNewEnhancedRandomBuilder().build();
+		Car car = enhancedRandom.nextObject(Car.class);
+		System.out.println(car.getBrand());
+		
 	}
 	
 	
